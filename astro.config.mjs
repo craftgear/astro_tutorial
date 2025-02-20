ef92192a83e2f1794b36f992bf173fd15e2c7842
Shunsuke Watanabe <craftgear@gmail.com>
2025-02-20 10:24:33 +0900

    clean slate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
-
-import mdx from '@astrojs/mdx'
-
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
-  experimental: {
-    svg: {
-      mode: 'inline',
-    },
-  },
-})
-
+export default defineConfig({});
